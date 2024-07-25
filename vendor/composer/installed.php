1c018a529741436c12b1330d699dad3ca2e39289
Ramadhanu Kadir Rachono <90473066+Danu710@users.noreply.github.com>
2024-07-25 20:14:32 +0700

    add composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'user/sismditp-web',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '604b4ee9b632922c6807eb751c686dd087be66ca',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'user/sismditp-web' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '604b4ee9b632922c6807eb751c686dd087be66ca',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
