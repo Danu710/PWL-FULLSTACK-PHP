635ed2ae87f5eae1ff3dc4f5b834bfce9552b57b
Ramadhanu Kadir Rachono <90473066+Danu710@users.noreply.github.com>
2024-07-25 21:18:50 +0700

    rename spp

--- spp.php
+++ spp.php
@@ -38,7 +38,7 @@ $json_spp = json_encode($spp);
 <div class="col-lg-9 mt-2">
     <div class="card">
         <div class="card-header">
-            Halaman SPP
+            Pages SPP
         </div>
         <div class="card-body">
         <!-- Modal tambah Siswa baru -->
