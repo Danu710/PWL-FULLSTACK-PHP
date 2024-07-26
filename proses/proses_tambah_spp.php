ef7081998f50969664e540be3c6ec0a809099d2e
Ramadhanu Kadir Rachono <90473066+Danu710@users.noreply.github.com>
2024-07-26 23:13:14 +0700

    proses tambah php

--- proses/proses_tambah_spp.php
+++ proses/proses_tambah_spp.php
@@ -9,10 +9,11 @@ $kelas = (isset($_POST['kelas'])) ? htmlentities($_POST['kelas']) : "";
 $bulan = (isset($_POST['bulan'])) ? htmlentities($_POST['bulan']) : "";
 $tanggal = date("Y-m-d");
 $bayar = (isset($_POST['bayar'])) ? htmlentities($_POST['bayar']) : "";
-$status = (isset($_POST['status'])) ? htmlentities($_POST['status']) : "";
+$status = (isset($_POST['transaction_status'])) ? htmlentities($_POST['transaction_status']) : "";
 $order_id = rand();
+$transaction_id="";
 
-mysqli_query($conn, "INSERT INTO spp VALUES ('$nipd', '$nama_siswa', '$kelas', '$bulan', '$bayar', '$tanggal', '$status', '$order_id')");
+mysqli_query($conn, "INSERT INTO spp VALUES ('$nipd', '$nama_siswa', '$kelas', '$bulan', '$bayar', '$tanggal', '$status', '$order_id', '$transaction_id')");
 
 
 header("Location: ../midtrans/examples/snap/checkout-process-simple-version.php?order_id=$order_id");
